555ef19852d4af617990a0dea187e93f9e213952
minhyup <minhyupp@gmail.com>
2021-02-19 15:12:26 +0900

    main버튼추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
-import MyContainer from '../components/containers/my/MyContainer';
-
+import { darken, lighten } from 'polished';
+//import MyContainer from '../components/containers/my/MyContainer';
+import styled from '@emotion/styled';
+const Button = styled.button`
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  height: 3rem;
+  font-size: 1rem;
+  /* 색상 */
+  background: #228be6;
+  &:hover {
+    background: ${lighten(0.1, '#228be6')};
+  }
+  &:active {
+    background: ${darken(0.1, '#228be6')};
+  }
+`;
 export default function Home(): React.ReactElement {
   return (
     <div className={styles.container}>
@@ -11,7 +33,10 @@ export default function Home(): React.ReactElement {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <MyContainer />
+      {/* <MyContainer /> */}
+      <Link href="/my">
+        <Button>Home으로 이동</Button>
+      </Link>
     </div>
   );
 }
